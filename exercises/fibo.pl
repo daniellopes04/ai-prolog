ae2151d215eaaaa094531f6f0e6bf681f45fe994
daniellopes04 <daniel.lopes04@gmail.com>
2021-04-08 11:43:27 -0300

    Adicionando exercicios

--- exercises/fibo.pl
+++ exercises/fibo.pl
@@ -1,17 +1,12 @@
-% Primeiro calculamos o numero F(n) de Fibonacci
+% Retorna o n-esimo elemento da sequencia de Fibonacci
 
-fibonacci(0,0) :- !.
-fibonacci(1,1) :- !.
+fibo(1,0).
+fibo(2,1).
 
-fibonacci(N,S) :- 
-    N > 1,
+fibo(N,S) :- 
+    	N > 2,
 	F1 is N-1,
 	F2 is N-2,
-	fibonacci(F1,S1),
-	fibonacci(F2,S2),
+	fibo(F1,S1),
+	fibo(F2,S2),
 	S is S1 + S2.
-
-% O n-esimo elemento de fibonnaci = F(n-1)
-
-fibo(N,S) :-
-    fibonacci(N-1,S).
