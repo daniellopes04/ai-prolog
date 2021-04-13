1e325c3ec7423b80996ce2a69129080f21d97f48
Daniel Lopes <daniel.lopes04@gmail.com>
2021-04-13 11:54:25 -0300

    Update list_operations.pl

--- prolog-bratko/chapter3/list_operations.pl
+++ prolog-bratko/chapter3/list_operations.pl
@@ -1,32 +1,56 @@
-% Operacoes em listas
+% List operations
 
-% Verifica se X e membro da lista L
+% Checks if X is member of list L
 member(X,[X|_]).
 member(X,[_|L]) :-
     member(X,L).
 
-% Concatena duas listas L1 e L2
+% Concatenate two lists L1 and L2
 append([],L,L).
 append([X|L1],L2,[X|L3]) :-
     append(L1,L2,L3).
 
-% Verifica se X e membro da lista L usando append
+% Checks if X is member of list L using append
 member1(X,L) :-
     append(_,[X|_],L).
 
-% Deleta os tres ultimos elementos de L
+% Delete the last three elements of L
 deleteLast3(L,R) :-
     append(R,[_,_,_],L).
 
+% Delete the first three elements of L
 deleteFirst3(L,R) :-
     append([_,_,_],R,L).
 
-% Deleta os tres primeiros e os tres ultimos elementos de L
+% Delete the first and last three elements of L
 deleteFirstLast3(L,R) :-
-    append([_,_,_],R,L),
-    append(R,[_,_,_],L).
+    deleteFirst3(L,R1),
+    deleteLast3(R1,R).
 
+% Checks if X is last member of L using append
 last(X,L) :-
-    append([_],X,L).
+    append(_,[X],L).
 
+% Checks if X is last member of L
+last2(X,[X]).
+
+last2(X,[_|L]) :-
+    last2(X,L).
+
+% Checks if list lenght is even
+evenlength([]).
+
+evenlength([_|L]) :-
+    oddlength(L).
+
+% Checks if list lenght is odd
+oddlength([_|L]) :-
+    evenlength(L).
+
+% Reverses a given list L into a reversed list RL
+reverse([L],L).
+
+reverse([X|L],RL) :-
+    reverse(L,Z),
+    append(Z,[X],RL).
     
