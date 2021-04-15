8653f7e00b9632a21a888b91054b60a7800f5f2e
Daniel Lopes <daniel.lopes04@gmail.com>
2021-04-15 20:06:45 -0300

    Create insert_orderedList.pl

---
+++ exercises/insert_orderedList.pl
@@ -0,0 +1,16 @@
+% Adiciona um elemento a uma lista ordenada
+
+% Caso base: inserindo o elemento numa lista vazia
+addOrdered(X,[],[X]).       
+
+% Caso 1: o elemento é menor do que o cabeça da lista
+% Inserimos o elemento no começo da lista
+addOrdered(X,[Head|Tail],[X,Head|Tail]) :-	
+    X =< Head.								              
+
+% Caso 2: o elemento é maior do que o cabeça da lista
+% Nesse caso, sabemos que o cabeça da lista continuará como tal
+% E vamos chamar novamente a função com a cauda da lista
+addOrdered(X,[Head|Tail],[Head|Tail2]) :-	  
+    X > Head,								                
+    addOrdered(X,Tail,Tail2).				        
